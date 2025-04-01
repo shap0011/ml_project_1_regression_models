bfa843b24411b7726ca34c1469cdc2ac03d7bfb0
shap0011 <shap0011@algonquinlive.com>
2025-04-01 03:25:21 -0400

    adds comments

--- regression_app.py
+++ regression_app.py
@@ -1,10 +1,20 @@
+# import Streamlit for building web app
 import streamlit as st
+# import pandas for data manipulation
 import pandas as pd
+#import numpy as np for numerical computing
+import numpy as np
 
+
+# set the title of the Streamlit app
 st.title('Project 1. Regression Models')
 
+# display a brief description of the app
 st.write('This app builds a machine learning model')
 
+# load the dataset from a CSV file located in the 'data' folder
 df = pd.read_csv('data/final.csv')
-# df
+
+# display the first five rows of the dataset in the app
+st.write('The dataset is loaded. The first five records displayed below:')
 st.write(df.head())
