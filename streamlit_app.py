7e8f1cb8eb595c617bb6473b2548aa664c95d872
shap0011 <55406914+shap0011@users.noreply.github.com>
2025-04-01 02:31:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 st.title('Project 1. Regression Models')
 
-st.write('This app builds a machine mearning model')
+st.write('This app builds a machine learning model')
 
 df = pd.read_csv('data/final.csv')
 # df
-df.head()
+st.write(df.head())
